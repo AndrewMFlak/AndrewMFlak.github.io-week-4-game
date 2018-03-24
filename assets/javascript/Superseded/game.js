1907520b6495b3b0896fe64e81c4684d3ee031bb
AFlak <AndrewMFlak@gmail.com>
2018-03-24 00:56:37 -0400

    initial add of files for crystalcollector

---
+++ assets/javascript/Superseded/game.js
@@ -0,0 +1,30 @@
+var jedi= {
+    name:"jedi",
+    hp: 100,
+    agility: 70,
+    force: 100,
+    blaster: 0,
+    rock: 0,
+};
+var wookie={
+    name: "wookie",
+    hp: 200,
+    agility: 60,
+    force: 10,
+    blaster: 100
+};
+var ewok={
+    name: "ewok",
+    hp: 60,
+    force: 40,
+    blaster:50,
+    rock: 120,
+};
+var rebel = {
+    name: "fatRebel",
+    hp: 80,
+    force: 30,
+    blaster: 120,
+    rock: 15,
+};
+
---
+++ assets/javascript/crystalCollector.js
@@ -0,0 +1,15 @@
+
+$( document).ready(function() {
+    var randomNumber = Math.floor(Math.random() * 100 + 27
+});
+
+var yourScore = 0;
+var randomNumber = Math.floor(Math.random() * 100);
+var wins = 0;
+var losses = 0;
+
+function initializeCrystal {
+    yourScore = "";
+    randomNumber = "";
+    isOperatorChose = "";
+}
